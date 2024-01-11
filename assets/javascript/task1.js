45760eb617400eec8fb8f2533a21fcbf9f2ff557
13442 <ryan.gilmer@vtsd.com>
2024-01-11 11:45:58 -0500

    dw

--- assets/javascript/task1.js
+++ assets/javascript/task1.js
@@ -1,1 +1,10 @@
 /* Put code for the task below */
+function clickButton(){
+    const button = document.getElementById("buttonTask1");
+    button.style.display = "none";
+}
+
+function clickImage(){
+    const image = document.getElementById("imageTask1");
+    image.style.display = "block";
+}
--- assets/javascript/task5.js
+++ assets/javascript/task5.js
@@ -1,1 +1,38 @@
 /* Put code for the task below */
+const submit = document.getElementById("submit");
+const fullName = document.getElementById("fullName");
+const address = document.getElementById("address");
+const phone = document.getElementById("phone");
+const email = document.getElementById("email");
+
+
+submit.addEventListener("click", my_function);
+function my_function() {
+
+    if (fullName.value === '') {
+        fullName.style.backgroundColor = "red";
+    } else {
+        fullName.style.backgroundColor = 'white';
+    }
+
+
+    if (address.value === '') {
+        address.style.backgroundColor = "red";
+    } else {
+        address.style.backgroundColor = 'white';
+    }
+
+
+    if (phone.value === '') {
+        phone.style.backgroundColor = "red";
+    } else {
+        phone.style.backgroundColor = 'white';
+    }
+
+
+    if (email.value === '') {
+        email.style.backgroundColor = "red";
+    } else {
+        email.style.backgroundColor = 'white';
+    }
+}
